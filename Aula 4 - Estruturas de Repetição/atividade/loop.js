c5159a77cac9d8ae7421f51256858d6e405f2eb5
claulis <cla_ulis@hotmail.com>
2024-03-19 09:35:40 -0300

    ok

---
+++ Aula 4 - Estruturas de Repetição/atividade/loop.js
@@ -0,0 +1,7 @@
+/*Repetir 10 vezes a frase olá mundo usando alert*/
+let fim =prompt("Digite um numero inteiro");
+for(let num=0;num<fim;num++)
+{//usamos let pois i é uma variável local
+ //interna ao for
+    alert("Olá mundo! "+(num+1));
+}
